dbbf20c499d3f5bd2ca06460d881f705259c6847
casper <casper@OUZHAN-PC>
2024-02-21 21:19:30 +0300

    Proje dosyası ekle.

---
+++ Ders2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Byte veri tipi
+            byte birinci = 5;
+
+            byte ikinci = 12;
+
+            byte ucuncu = 255; //0-255
+
+            byte maxDeger = byte.MaxValue;
+            byte minDeger = byte.MinValue;
+
+            Console.WriteLine(minDeger);
+            Console.WriteLine(maxDeger);
+            Console.ReadLine(); 
+
+            + Short veri tipi
+            -32768 ile 3267 arasında değer tutabilir.
+            short a = -50;
+            short a2 = 25550;
+            short a3 = 32000;
+            short maxDeger = short.MaxValue;
+            short minDeger = short.MinValue;
+            Console.WriteLine(maxDeger);
+            Console.WriteLine(minDeger);
+            Console.ReadLine(); 
+
+            +integer veri tipi -2.147.483.648 ile 2.147.483.647 arasında değer tutar
+            4 byte yer kaplar
+            int a1 = 150000;
+            int a2 = -500000;
+            int maxDeger = int.MaxValue;
+            int minDeger = int.MinValue;
+
+            Console.WriteLine(maxDeger);
+            Console.WriteLine(minDeger);
+            Console.ReadLine();
+
+            +Long veri tipi -9,223,372,036,854,755,808 to 9,223,372,036,854,775,007 değer tutabilir
+            8 byte yer kaplar ram bellekte.
+
+            long a = 575000;
+            long a2 = -500;
+            long maxDeger = long.MaxValue;
+            long minDeger = long.MinValue;
+            Console.WriteLine(maxDeger);
+            Console.WriteLine(minDeger);
+            Console.ReadLine();
+
+            +float
+            4byte
+            float ondalikliSayi = 35.56f;
+            float ondalikliSayi2 = 12.5f;
+            Console.WriteLine(ondalikliSayi);
+            Console.WriteLine(ondalikliSayi2);
+            Console.ReadLine();
+
+            +double 
+            8 byte yer kaplar
+            15 basamaklı ondalıklı sayılarıda tutabilir.
+            double a = 350.7;
+            double a2 = -250.6;
+            Console.WriteLine(a);
+            Console.WriteLine(a2);
+            Console.ReadLine();
+
+            +char veri tipi
+            Karakterleri tutmak için char veri tipi kullanırız.
+            Tek tırnak içerisinde tek değer olacak şekilde kullanılır.
+            char karakterPaneli = 'U';
+            Console.WriteLine(karakterPaneli);
+            Console.ReadLine();
+
+            +String
+            string login = "Welcome Back";
+            Console.WriteLine(login);
+            Console.ReadLine();
+
+            +Boolean 
+            bool check = true;
+            bool check2 = false;
+            bool result = 10 > 5;
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            +Object
+            Object veri bütün veri tiplerini tutar, veri tiplerinin allahıdır.
+            object a =  "Checking your chrachter";
+       */ }
+   */ }
+*/ }
